cd96c23837e5853ff80b1af9e46d73d299aa496a
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-05 00:09:15 +0530

    Create Submit forn function and snackBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:data_app/controller.dart';
+import 'package:data_app/models/feedback_form.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -37,15 +39,55 @@ class _HomePageState extends State<HomePage> {
   TextEditingController mobilenoController = TextEditingController();
   TextEditingController feedbackController = TextEditingController();
 
+  void _submitForm(){
+    if(_formKey.currentState.validate()){
+      FeedbackForm feedbackForm = FeedbackForm(
+        nameController.text,
+        emailController.text,
+        mobilenoController.text,
+        feedbackController.text
+      );
 
+      FormController formController= FormController(
+        (String response){
+          print(response);
+          if(response==FormController.STATUS_SUCCESS){
+            _showSnakBar("Feedback Submitted");
+          }else{
+            _showSnakBar("Error in feedback");
+          }
+        }
+      );
+      _showSnakBar("Submitting Feedback");
+      formController.submitForm(feedbackForm);
+    }
+    
+  }
+
+  _showSnakBar(String message){
+    final snackBar = SnackBar(
+      content: Text(message),
+    );
+    _scaffoldKey.currentState.showSnackBar(snackBar);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Form( 
+        key: _formKey,
         child: Container(
+          padding: EdgeInsets.symmetric(horizontal:24, vertical:100),
           child: Column(
             children: <Widget>[
+              Text(
+                "GoogleSheet",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 30,),
               TextFormField(
                 decoration: InputDecoration(
                   hintText: "Name",
