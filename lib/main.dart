878ad4762c6226ae114a8eb4acaf35fe0eb8f7b8
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-04 23:52:46 +0530

    Create Forn

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
+        
         primarySwatch: Colors.blue,
 
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -27,10 +28,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController mobilenoController = TextEditingController();
+  TextEditingController feedbackController = TextEditingController();
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Form( 
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Name",
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Email",
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Mobile Number",
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Feedback",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
